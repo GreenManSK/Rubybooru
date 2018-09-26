a48703b2d77bb2f0f6757b91daeeca3dc2875274
Lukáš Kurčík <lukas.kurcik@gmail.com>
2018-09-26 22:31:04 +0200

    Design of grids

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  faSearch = faSearch;
+
+  arrayOne( n: number ): any[] {
+    return Array(n);
+  }
 }
